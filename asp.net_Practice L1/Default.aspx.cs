0ae7d3ab963f515a136b4e920cda0d5cf91c33d5
janelle-nomad <janellenomad@gmail.com>
2016-06-21 22:14:49 -0400

    Created a basic ASP.NET Web form Version 0.1 - Created  form with bootstrap, Font Awesome, JQuery

---
+++ asp.net_Practice L1/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp.net_Practice_L1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            FullNameTb.Text = FirstNameTb.Text.ToString() + " " + LastNameTb.Text.ToString();
+        }
+    }
+}
